e1d1732c4334a418a5af2c65fc8497932d93ed38
realchoky <realchoky@gmail.com>
2024-09-10 16:36:02 +0900

    123

---
+++ springbootbasic/src/main/java/springbootbasic/service/PersonService.java
@@ -0,0 +1,12 @@
+package springbootbasic.service;
+import java.util.List;
+
+import springbootbasic.domain.Person;
+
+public interface PersonService {
+	
+	public abstract Person getPerson();
+	
+	public abstract List<Person> getPersonList();
+
+}
